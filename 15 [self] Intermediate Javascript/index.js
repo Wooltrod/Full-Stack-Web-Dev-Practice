aac95298e27f12ef12703b5470d8231bf0b9de03
Wooltrod <ianadoyo64@gmail.com>
2025-02-21 20:15:10 +0100

    Javascript code for a pseudo random number generator for the outcome of a rolled die

---
+++ 15 [self] Intermediate Javascript/index.js
@@ -0,0 +1,9 @@
+/*pseudo random number generator 
+for simulating a die roll*/
+//"randomly" creates a random 16-place decimal between 0 - 0.9999999999999999
+var decimal = Math.random();
+//we multiply by '6' to fit the figure within the parameters of the die (6 outcomes)
+var realNumber = decimal * 6;
+//we take the floor of the above value to obtain a clean integer
+var dieInteger = Math.floor(realNumber) + 1; 
+console.log(dieInteger);
