8e68c29acdeb6b3838efcdf26f40969ca05102d3
Wooltrod <ianadoyo64@gmail.com>
2025-02-21 20:32:45 +0100

    The Love Calculator (returns a random, unsubstantiated compatibility percentage after a user submits their name and their crush's name)

--- 15 [self] Intermediate Javascript/index.js
+++ 15 [self] Intermediate Javascript/index.js
@@ -13,3 +13,11 @@ var n = Math.random();
 n = n * 6;
 n = Math.floor(n) + 1;
 console.log(n);
+
+//The Love Calculator
+var person1 = prompt("Write your first name:");
+var person2 = prompt("Write your crush's first name:");
+var fakePercentage = Math.random();
+fakePercentage = fakePercentage * 100;
+fakePercentage = Math.floor(fakePercentage) + 1;
+alert(person1 + "'s love compatibility with " + person2 + " is: " + fakePercentage + "%.");
