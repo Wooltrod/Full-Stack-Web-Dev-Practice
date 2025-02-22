02c3b3c83050e163a4ca6d55d2f37005159e3776
Wooltrod <ianadoyo64@gmail.com>
2025-02-22 12:53:26 +0100

    added more functionality to the Love Calculator by applying control flow (if-else) to append specific messages for specific range of percentages

--- 15 [self] Intermediate Javascript/index.js
+++ 15 [self] Intermediate Javascript/index.js
@@ -21,3 +21,16 @@ var fakePercentage = Math.random();
 fakePercentage = fakePercentage * 100;
 fakePercentage = Math.floor(fakePercentage) + 1;
 alert(person1 + "'s love compatibility with " + person2 + " is: " + fakePercentage + "%.");
+
+//CONTROL FLOW
+//The Love Calculator (with specific messages for specific percantages; C)
+var person1 = prompt("Write your first name:");
+var person2 = prompt("Write your crush's first name:");
+var fakePercentage = Math.random();
+fakePercentage = fakePercentage * 100;
+fakePercentage = Math.floor(fakePercentage) + 1;
+if (fakePercentage > 70){
+    alert(person1 + "'s love compatibility with " + person2 + " is: " + fakePercentage + "%. You love each other like Kanye Loves Kanye");
+} else {
+    alert(person1 + "'s love compatibility with " + person2 + " is: " + fakePercentage + "%.");
+}
