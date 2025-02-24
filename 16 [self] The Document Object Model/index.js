2766889d2466cf82c64101e8198bfc120c3413c4
Wooltrod <ianadoyo64@gmail.com>
2025-02-24 18:09:19 +0100

    Added JS code that illustrates changing H1 element of a webpage, using Javascript

--- 16 [self] The Document Object Model/index.js
+++ 16 [self] The Document Object Model/index.js
@@ -2,3 +2,11 @@ document; //selects entire HTML Page
 document.firstElementChild; //selects the <html>...</html> element and everything within it
 document.firstElementChild.firstElementChild; //usually returns the Head Element + its contents
 document.firstElementChild.lastElementChild; //usually returns the body element + its contents
+
+document.firstElementChild.lastElementChild.firstElementChild; //let's say this brings us to a H1
+
+//...we can save it in a Javascript variable:
+var heading = document.firstElementChild.lastElementChild.firstElementChild;
+
+//we can then target the heading variable with functions to modify our H1 as we wish:
+heading.innerHTML = "Good Bye"; //"innerHTML" denotes the part between the given HTML tags
