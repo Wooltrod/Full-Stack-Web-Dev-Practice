472e393546300155030d2f965fe67f69764e0a6b
Wooltrod <ianadoyo64@gmail.com>
2025-02-24 17:59:47 +0100

    Added sample javascript commands demonstrating the selection of specific elements of a document

---
+++ 16 [self] The Document Object Model/index.js
@@ -0,0 +1,4 @@
+document; //selects entire HTML Page
+document.firstElementChild; //selects the <html>...</html> element and everything within it
+document.firstElementChild.firstElementChild; //usually returns the Head Element + its contents
+document.firstElementChild.lastElementChild; //usually returns the body element + its contents
