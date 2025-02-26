130c160fe54f512f5ef32e89793f2d0728b7912f
Wooltrod <ianadoyo64@gmail.com>
2025-02-26 22:31:23 +0100

    Added control flow (if - else conditions) dictating the displayed image for each outcome of the random number generator for Player 1's dice.

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,1 +1,15 @@
 var randomNumberDieOne = (Math.floor(Math.random()*6)) + 1;
+
+if(randomNumberDieOne === 1){
+    document.querySelector(".dice > .img1").setAttribute("src", "./images/dice1.png");
+}else if(randomNumberDieOne === 2){
+    document.querySelector(".dice > .img1").setAttribute("src", "./images/dice2.png");
+}else if(randomNumberDieOne === 3){
+    document.querySelector(".dice > .img1").setAttribute("src", "./images/dice3.png");
+}else if(randomNumberDieOne === 4){
+    document.querySelector(".dice > .img1").setAttribute("src", "./images/dice4.png");
+}else if(randomNumberDieOne === 5){
+    document.querySelector(".dice > .img1").setAttribute("src", "./images/dice5.png");
+}else if(randomNumberDieOne === 6){
+    document.querySelector(".dice > .img1").setAttribute("src", "./images/dice6.png");
+}
