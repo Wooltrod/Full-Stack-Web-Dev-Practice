b898ebba75d81d665d1cdb5012cb8a505e6908b2
Wooltrod <ianadoyo64@gmail.com>
2025-02-28 21:45:47 +0100

    Replaced element selector "button" with class selector ".drum" to specifically target button elements within the specific div

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,6 +2,6 @@ function handleClick(){
     alert("I got clicked!");
 }
 
-for(var i = 0; i < document.querySelectorAll("button").length ; i++){
-    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+for(var i = 0; i < document.querySelectorAll(".drum").length ; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
