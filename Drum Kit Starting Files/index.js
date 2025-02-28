0f82132d064dbf8a2a9425fac6f35b8367590b03
Wooltrod <ianadoyo64@gmail.com>
2025-02-28 21:17:34 +0100

    Created JS code that adds the event listener to all button elements in the HTML

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,5 +1,7 @@
-document.querySelector("button").addEventListener("click", handleClick);
-
 function handleClick(){
     alert("I got clicked!");
+}
+
+for(var i = 0; i < document.querySelectorAll("button").length ; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
 }
