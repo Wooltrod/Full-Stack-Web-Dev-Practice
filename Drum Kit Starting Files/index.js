6d15dc31513aaafb23d4c07082165b05b4f93075
Wooltrod <ianadoyo64@gmail.com>
2025-03-01 09:36:09 +0100

    Alternative implementation, using Anonymous functions to specify the event listener

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -11,6 +11,14 @@ for(var i = 0; i < document.querySelectorAll(".drum").length ; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", play);
 }
 
+//alternative implementation:
+for(var i = 0; i < document.querySelectorAll(".drum").length ; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        var audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
+    });
+}
+
 //Higher order functions
 function multiply(num1, num2){return num1 * num2;}
 function divide(num1, num2){return num1 / num2;}
