8a2ea0004631358c762fee9a8aa30106ab5f6b79
Wooltrod <ianadoyo64@gmail.com>
2025-03-01 09:12:58 +0100

    Added sample implementation code showcasing High Order Functions (Calculator App)

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -5,3 +5,13 @@ function handleClick(){
 for(var i = 0; i < document.querySelectorAll(".drum").length ; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
+
+//Higher order functions
+function multiply(num1, num2){return num1 * num2;}
+function divide(num1, num2){return num1 / num2;}
+function subtract(num1, num2){return num1 - num2;}
+function add(num1, num2){return num1 + num2;}
+
+function calculator(num1, num2, operator){
+    return operator(num1, num2);
+}
