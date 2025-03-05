67c0e5533686438bf0d7d72afd827377eb3742c3
Wooltrod <ianadoyo64@gmail.com>
2025-03-05 21:58:23 +0100

    Website animations with jQuery (chaining is used in this case)

--- jQuery/index.js
+++ jQuery/index.js
@@ -75,3 +75,8 @@ $("h1").before("<button>New</button>");
 $("h1").before("<button>New</button>");
 $("h1").prepend("<button>New</button>");
 $("h1").append("<button>New</button>");
+
+//Website animations with jQuery:
+$("button").on("click", function(){
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+});
