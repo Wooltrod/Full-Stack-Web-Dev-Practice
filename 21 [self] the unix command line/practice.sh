5b64424ba0061a94cd8e6c84fc5e74aacc5c8190
Wooltrod <ianadoyo64@gmail.com>
2025-03-07 23:17:13 +0100

    Added bash comments (notes) on fundamental commands and keybord shortcuts for navigating the BASH CLI

---
+++ 21 [self] the unix command line/practice.sh
@@ -0,0 +1,11 @@
+#ls: listing contents of a directory
+#~: indicates the root "user" directory
+#cd: "change directory"
+#cd Documents/Learn/Languages: navigates to the language folder, following the hierarchical path
+#cd ~: takes us back to the root directory location/"user" folder
+#cd ..: takes us back one level within the hierarchy
+
+#Shortcuts for lines of commands:
+#CTRL + A: beginning of the command
+#CTRL + E: end of the command
+#CTRL + U: clears the command entirely
