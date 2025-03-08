80a25a667899d55b717e531e91ae00cfae4f989f
Wooltrod <ianadoyo64@gmail.com>
2025-03-08 12:30:20 +0100

    Some additional BASH Scripting commands

--- 21 [self] the unix command line/practice.sh
+++ 21 [self] the unix command line/practice.sh
@@ -9,3 +9,14 @@
 #CTRL + A: beginning of the command
 #CTRL + E: end of the command
 #CTRL + U: clears the command entirely
+
+#---
+#mkdir + [folder name]: "make directory", used to create a directory/folder within the current directory
+#touch + [file_name.file_extension]: command for creating a file (specifying file extension necessary)
+#open + [file_name.file_extension]: Opens an existing file located within the current directory
+#open -a + [application_file_path.app_extension] + [file_name.file_extension]: opening a specific file within a specific application
+#rm [file_name.file_extension]: deleting a specific file within current directory
+#rm *: deleting all files within current directory
+
+#pwd: command for verifying you current actual location
+#rm -r [folder_name]: command for removing directory/folder found within current directory
