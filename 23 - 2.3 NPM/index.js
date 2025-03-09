d93cc9834eaea272b364a435cc1d83c639808f78
Wooltrod <ianadoyo64@gmail.com>
2025-03-09 14:40:35 +0100

    final js code for implementing the "superheroes" package

--- 23 - 2.3 NPM/index.js
+++ 23 - 2.3 NPM/index.js
@@ -1,6 +1,9 @@
 // var generateName = require("sillyname");
 
-import generateName from "sillyname";
-var sillyName = generateName();
+// import generateName from "sillyname";
+// var sillyName = generateName();
 
-console.log(`My name is ${sillyName}.`);
+import { randomSuperhero } from "superheroes";
+const superheroName = randomSuperhero();
+
+console.log(`My name is ${superheroName}.`);
