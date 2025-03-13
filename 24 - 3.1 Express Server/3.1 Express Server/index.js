82e88b5b5ea9eff5a1059720311642c86b1fb83d
Wooltrod <ianadoyo64@gmail.com>
2025-03-13 18:33:37 +0100

    initial setup of our server application

--- 24 - 3.1 Express Server/3.1 Express Server/index.js
+++ 24 - 3.1 Express Server/3.1 Express Server/index.js
@@ -1,1 +1,6 @@
-npm
+import express from "express"; //imports the express object from the express module that we just installed
+const app = express(); //create an app using the express object
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000.")
+})
