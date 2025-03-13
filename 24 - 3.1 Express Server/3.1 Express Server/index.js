11c15f698b7964dd4cecf97e640068f61777cac7
Wooltrod <ianadoyo64@gmail.com>
2025-03-13 18:38:18 +0100

    More preferred method, passing the port number as a variable

--- 24 - 3.1 Express Server/3.1 Express Server/index.js
+++ 24 - 3.1 Express Server/3.1 Express Server/index.js
@@ -1,6 +1,12 @@
 import express from "express"; //imports the express object from the express module that we just installed
 const app = express(); //create an app using the express object
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000.")
+// app.listen(3000, () => {
+//     console.log("Server running on port 3000.")
+// });
+
+//variable method for port number:
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`)
 });
