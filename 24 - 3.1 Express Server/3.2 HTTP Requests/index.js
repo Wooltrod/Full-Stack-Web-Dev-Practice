458072366d50917501f79b374fc91aafd703c4cc
Wooltrod <ianadoyo64@gmail.com>
2025-03-13 19:37:08 +0100

    Challenge: defined how the server should respond to the client request

--- 24 - 3.1 Express Server/3.2 HTTP Requests/index.js
+++ 24 - 3.1 Express Server/3.2 HTTP Requests/index.js
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 const port = 3001;
 
+app.get("/", (req, res) =>{
+    res.send("Hello, Ian!")
+});
+
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
 });
