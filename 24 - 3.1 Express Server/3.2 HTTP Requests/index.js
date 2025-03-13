edd9e0ad9bbb834461d3f864ad9afa88c8f0ff3b
Wooltrod <ianadoyo64@gmail.com>
2025-03-13 19:53:52 +0100

    Added server responses (HTML pages) for various endpoints (specified by the user in the url, e.g. localhost:3000/about)

--- 24 - 3.1 Express Server/3.2 HTTP Requests/index.js
+++ 24 - 3.1 Express Server/3.2 HTTP Requests/index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3001;
 
 app.get("/", (req, res) =>{
-    res.send("Hello, Ian!")
+    res.send("<h1>Hello, Ian!</h1>");
+});
+app.get("/contacts", (req, res) =>{
+    res.send("<h1>Contact us</h1>");
+});
+app.get("/about", (req, res) =>{
+    res.send("<h1>About our company</h1>");
 });
 
 app.listen(port, () => {
