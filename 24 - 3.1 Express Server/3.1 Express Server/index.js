7322b145db64a885fd45e7db4806e42e276f6064
Wooltrod <ianadoyo64@gmail.com>
2025-03-13 19:10:17 +0100

    Defined the actions the server should take when it receives the client request (localhost:3000 via browser)

--- 24 - 3.1 Express Server/3.1 Express Server/index.js
+++ 24 - 3.1 Express Server/3.1 Express Server/index.js
@@ -1,6 +1,13 @@
 import express from "express"; //imports the express object from the express module that we just installed
 const app = express(); //create an app using the express object
 
+
+//actions the server should take when it receives the client request
+//the client request in this case is triggered through the address "localhost:3000"
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
 // app.listen(3000, () => {
 //     console.log("Server running on port 3000.")
 // });
