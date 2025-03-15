67704dcc26e8aa61b6c5350e965cb3bc77aea2c1
Wooltrod <ianadoyo64@gmail.com>
2025-03-15 11:56:58 +0100

    Completed implementation of Morgan middleware, prints logging info on console using "req.body"

--- 24 - 3.4 Middleware/index2.js
+++ 24 - 3.4 Middleware/index2.js
@@ -1,10 +1,13 @@
 import express from "express";
+import morgan from "morgan";//importing the Morgan logging module
 
 const app = express();
 const port = 3000;
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.send("Hello");
+  console.log(req.body);
 });
 
 app.listen(port, () => {
