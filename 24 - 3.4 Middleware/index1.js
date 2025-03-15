95e1d10169d9b9ecd001f61e5f8e55316d469425
Wooltrod <ianadoyo64@gmail.com>
2025-03-15 11:19:26 +0100

    Completed the body-parser implementation task (middleware)

--- 24 - 3.4 Middleware/index1.js
+++ 24 - 3.4 Middleware/index1.js
@@ -1,13 +1,26 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";//importing the body-parser module
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+//mounting the middleware using the express ".use()" method
+/*specifying ".urlencoded({ extended: true})" to create a 
+body for URL-encoded requests like our own form submission*/
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+/*write a ".post("/submit")" handler where I console/log() the 
+for contents when the user clicks the submit button*/
+app.post("/submit", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
