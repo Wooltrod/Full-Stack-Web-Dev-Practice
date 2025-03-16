86499e4e94a864a338ee418e403825f224124984
Wooltrod <ianadoyo64@gmail.com>
2025-03-16 13:19:52 +0100

    Completed first EJS coding task
    npm dependencies: ejs, express, path, url, ?bodyParser.
    
    if-else control statements used.
    
    res.render() method used to pass the relevant parameters generated in the .js file to the ejs file.

--- 25 [self] 4.0 EJS/index.js
+++ 25 [self] 4.0 EJS/index.js
@@ -1,2 +1,27 @@
 import express from "express";
-import x from ejs;
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { escapeXML } from "ejs";
+import bodyParser from "body-parser";//importing the body-parser module
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+var today = new Date();
+var message;
+if (today.getDay() === 1 || today.getDay() === 2 || today.getDay() === 3 || today.getDay() === 4 || today.getDay() === 5 ){
+    message = "It's a weekday, it's time to work hard!";
+}else{
+    message = "It's the weekend, it's time to have fun";
+}
+
+app.get("/", (req, res) => {
+    res.render(__dirname + "/views/index.ejs", { messageDisplay: message });
+  });
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
