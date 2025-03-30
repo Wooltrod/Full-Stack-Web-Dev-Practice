ac7a0a10b7122884465d6927b4a3240e2bea9ae6
Wooltrod <ianadoyo64@gmail.com>
2025-03-30 22:47:02 +0200

    Add API JS code for GET route (GET All & GET Specific)

--- 30 - 6.1 Blog API Project/index.js
+++ 30 - 6.1 Blog API Project/index.js
@@ -41,8 +41,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
 
 //CHALLENGE 2: GET a specific post by id
+app.get("/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const foundPost = posts.find((post) => post.id === id);
+  res.json(foundPost);
+});
 
 //CHALLENGE 3: POST a new post
 
