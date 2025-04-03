3af3580a179ceeb685a9852be324edb96053bd8e
Wooltrod <ianadoyo64@gmail.com>
2025-04-03 23:33:17 +0200

    Insert Partial Record into a database INSERT INTO <table_name> (<fields you have data for>)

--- 32 [self] SQL/queries.sql
+++ 32 [self] SQL/queries.sql
@@ -5,5 +5,11 @@
     PRIMARY KEY (id)
 )*/
 
-INSERT INTO products
-VALUES (1, 'Pen', 1.20)
+/*INSERT INTO products
+VALUES (1, 'Pen', 1.20)*/
+
+--inserting a partial record (without price):
+--INSERT INTO <table_name> (<fields you have data for>)
+
+INSERT INTO products (id, name)
+VALUES (2, 'Pencil')
