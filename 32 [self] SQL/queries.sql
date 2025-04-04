37be709f17263903188ab7cd158ea3bf330ad060
Wooltrod <ianadoyo64@gmail.com>
2025-04-04 15:26:53 +0200

    Added sample queries for READ and SELECT commands

--- 32 [self] SQL/queries.sql
+++ 32 [self] SQL/queries.sql
@@ -11,5 +11,15 @@ VALUES (1, 'Pen', 1.20)*/
 --inserting a partial record (without price):
 --INSERT INTO <table_name> (<fields you have data for>)
 
-INSERT INTO products (id, name)
-VALUES (2, 'Pencil')
+/*INSERT INTO products (id, name)
+VALUES (2, 'Pencil')*/
+
+
+--selecting the entire database:
+SELECT * FROM products
+
+--selecting particular columns/fields:
+SELECT name, price FROM products
+
+--selecting particular rows (WHERE):
+SELECT * WHERE id=1
