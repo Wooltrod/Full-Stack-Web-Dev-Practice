6a6e828492c2c2939eedc74f23b8799fb9b4f18f
Wooltrod <ianadoyo64@gmail.com>
2025-04-09 21:11:29 +0200

    Completed Part 2 of the Travel Tracker Challenge.
    Note that SQL queries return arrays in Javascript. Always use '.rows' keyword to single out the records from the response.

--- 33 - 8.3 Travel Tracker/index.js
+++ 33 - 8.3 Travel Tracker/index.js
@@ -12,26 +12,38 @@ db.connect();
 
 let allVisitedCountries = [];
 
-db.query("SELECT * FROM visited_countries", (err, res) => {
-  if (err) {
-    console.error("Error executing query", err.stack);
-  } else {
-    allVisitedCountries = res.rows;
-  }
-  db.end();
-});
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  try{
+    await db.query("SELECT * FROM visited_countries", (err, res) => {
+      if (err) {
+        console.error("Error executing query", err.stack);
+      } else {
+        allVisitedCountries = res.rows;
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
   res.render("index.ejs", { 
     countries: allVisitedCountries.map(c => c.country_code),
     total: allVisitedCountries.length,
   });
+});
 
-  console.log(allVisitedCountries);
+app.post("/add", async (req, res) => {
+  const formattedUserInput = (req.body.country.trim()).slice(0,1).toUpperCase() + (req.body.country.trim()).slice(1,(req.body.country.trim()).length).toLowerCase();
+  console.log(formattedUserInput);
+  let matchingRecord = (await db.query("SELECT * FROM countries WHERE country_name = $1", [formattedUserInput])).rows;
+  console.log(matchingRecord);
+  console.log(matchingRecord[0].country_code);
+  db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [matchingRecord[0].country_code]);
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
