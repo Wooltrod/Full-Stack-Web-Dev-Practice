a279a8d032211ee89a0a6682384cf14327539f14
Wooltrod <ianadoyo64@gmail.com>
2025-04-17 23:12:47 +0200

    Update Index.js import bcrypt package; set variable for preferred number of salt rounds

--- 35 - 3.1 9.2 Authentication Lv.2/index.js
+++ 35 - 3.1 9.2 Authentication Lv.2/index.js
@@ -1,17 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { dbConfig } from "./dbConfig.js";
 import pg from "pg";
+import bcrypt from bcrypt;
 
 const app = express();
 const port = 3000;
+const saltrounds = 10;
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "123456",
-  port: 5432,
-});
+const db = new pg.Client(dbConfig);
+
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
