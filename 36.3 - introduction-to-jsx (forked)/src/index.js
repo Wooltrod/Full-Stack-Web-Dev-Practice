7f2c01f55888943cf157ad90331164991240fc70
Wooltrod <ianadoyo64@gmail.com>
2025-04-22 23:40:51 +0200

    Add comments

--- 36.3 - introduction-to-jsx (forked)/src/index.js
+++ 36.3 - introduction-to-jsx (forked)/src/index.js
@@ -1,5 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"; //var React = require("react");
+import ReactDOM from "react-dom"; //var ReactDOM = require("react-dom");
+
+/*ReactDOM.render() takes three inputs:
+WHAT TO SHOW (e.g. <h1>Hello World</h1>),
+WHERE TO SHOW IT (document.getElementById("root"),)*/
 
 ReactDOM.render(
   <div>
