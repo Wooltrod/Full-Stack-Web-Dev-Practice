860c7f1ba9f1a4eac7b914ad6363d9574e3ea1c1
Wooltrod <ianadoyo64@gmail.com>
2025-04-27 15:25:32 +0200

    Add 'React Styling Practice' Challenge folder

---
+++ 36.9 - React Styling Practice (forked)/src/index.js
@@ -0,0 +1,26 @@
+//Create a React app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+var timelyMessage = "Good Morning!";
+const currentTime = new Date().getHours();
+if (currentTime >= 0 && currentTime <= 11) {
+  timelyMessage = "Good Morning";
+} else if (currentTime >= 12 && currentTime <= 16) {
+  timelyMessage = "Good Afternoon!";
+} else {
+  timelyMessage = "Good Evening!";
+}
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">{timelyMessage}</h1>
+  </div>,
+  document.getElementById("root")
+);
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
