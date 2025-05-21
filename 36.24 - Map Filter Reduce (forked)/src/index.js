895769333d8fe4b273a07606162a77f7bb230bb9
Wooltrod <ianadoyo64@gmail.com>
2025-05-21 22:31:15 +0200

    Implement Map to return only array of meanings from our emojipedia.js object. Make use of string.substring() methos to truncate strings to desired character length

--- 36.24 - Map Filter Reduce (forked)/src/index.js
+++ 36.24 - Map Filter Reduce (forked)/src/index.js
@@ -1,38 +1,40 @@
 import emojipedia from "./emojipedia";
 var numbers = [3, 56, 2, 48, 5];
 
-//Map -Create a new array by doing something with each item in an array.
-function double(x){
-    return x * 2;
-}
+// //Map -Create a new array by doing something with each item in an array.
+// function double(x){
+//     return x * 2;
+// }
 
-const newNumbers =  numbers.map(double);
+// const newNumbers =  numbers.map(double);
 
-const newNumbers2 = numbers.map(function (x) {
-    return x * 2;
-});
+// const newNumbers2 = numbers.map(function (x) {
+//     return x * 2;
+// });
 
-//Filter - Create a new array by keeping the items that return true.
-const newNumbers3 = numbers.filter(function (num) {
-    return num > 10;
-});
+// //Filter - Create a new array by keeping the items that return true.
+// const newNumbers3 = numbers.filter(function (num) {
+//     return num > 10;
+// });
 
-//Reduce - Accumulate a value by doing something to each item in an array.
-const newNumbers4 = numbers.reduce(function (accumulator, currentNumber) {
-    return accumulator + currentNumber;
-});
+// //Reduce - Accumulate a value by doing something to each item in an array.
+// const newNumbers4 = numbers.reduce(function (accumulator, currentNumber) {
+//     return accumulator + currentNumber;
+// });
 
-//Find - find the first item that matches from an array.
-numbers.find(function (num) {
-    return num > 10;
-});
+// //Find - find the first item that matches from an array.
+// numbers.find(function (num) {
+//     return num > 10;
+// });
 
-//FindIndex - find the index of the first item that matches.
-numbers.findIndex(function (num) {
-    return num > 10;
-});
+// //FindIndex - find the index of the first item that matches.
+// numbers.findIndex(function (num) {
+//     return num > 10;
+// });
 
 //Challenge: Write code that returns only the 'meaning' fields of the objects from emojipedia, making sure that only up to the first 100 characters is rendered for each
 const meaningsArray = emojipedia.map(function (e) {
-    return e.meaning;
+    return (e.meaning).substring(0, 101);
 })
+
+console.log(meaningsArray);
