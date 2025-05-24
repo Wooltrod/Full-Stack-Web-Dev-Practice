3debe2b9624c3c325a3606d2e26a998245ddce17
Wooltrod <ianadoyo64@gmail.com>
2025-05-24 23:29:31 +0200

    Add ternary operator for Conditional rendering of certain components. 'isLoggedIn'

--- 36.27 - React Conditional Rendering (forked)/src/components/App.jsx
+++ 36.27 - React Conditional Rendering (forked)/src/components/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Login from "./Login";
 
+var isLoggedIn = false;
+
 function App() {
   return (
-    <div className="container">
-      <h1>Hello</h1>
+    <div className="container">{
+      isLoggedIn === true ? <h1>Hello</h1> :
       <form className="form">
         <Login buttonName="Login" />
       </form>
-    </div>
+    }</div>
   );
 }
 
