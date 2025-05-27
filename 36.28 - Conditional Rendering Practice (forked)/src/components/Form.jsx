3f2c5a3f1b056aa0b63311782dbe40328ae19182
Wooltrod <ianadoyo64@gmail.com>
2025-05-27 15:01:50 +0200

    [Angela's Solution] Configuring our 'isRegistered' prop which will be based on the boolean value of the 'userIsRegistered' in App.jsx

--- 36.28 - Conditional Rendering Practice (forked)/src/components/Form.jsx
+++ 36.28 - Conditional Rendering Practice (forked)/src/components/Form.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { userIsRegistered } from "./App";
 
-function Form(props) {
+function Form() {
   return (
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
       {userIsRegistered ? (
+        <button type="submit">Login</button>
+      ) : (
+        <div>
+          <input type="password" placeholder="Confirm Password" />
+          <button type="submit">Register</button>
+        </div>
+      )}
+    </form>
+  );
+}
+
+/*Angela's Solution (without using the imported userIsRegistered variable)*/
+function Form(props) {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      {props.isRegistered ? (
         <button type="submit">Login</button>
       ) : (
         <div>
