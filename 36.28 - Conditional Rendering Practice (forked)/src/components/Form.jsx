38d6f0d098a06cc305a106c80248cb6e64b641f2
Wooltrod <ianadoyo64@gmail.com>
2025-05-27 14:47:36 +0200

    Refactor Form.jsx to conditionally render certain components within itself, usin ternary operators and based on the imported { userIsRegistered } variable.

--- 36.28 - Conditional Rendering Practice (forked)/src/components/Form.jsx
+++ 36.28 - Conditional Rendering Practice (forked)/src/components/Form.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { userIsRegistered } from "./App";
 
-function Form() {
+function Form(props) {
   return (
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      {userIsRegistered ? (
+        <button type="submit">Login</button>
+      ) : (
+        <div>
+          <input type="password" placeholder="Confirm Password" />
+          <button type="submit">Register</button>
+        </div>
+      )}
     </form>
   );
 }
