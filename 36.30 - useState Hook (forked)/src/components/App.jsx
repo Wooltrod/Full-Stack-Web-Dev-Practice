30ecf2909bf4f7cff0dffec54dd78cb3baaefe02
Wooltrod <ianadoyo64@gmail.com>
2025-05-28 19:46:27 +0200

    Add 'decrease' button + functionality. Also comment.

--- 36.30 - useState Hook (forked)/src/components/App.jsx
+++ 36.30 - useState Hook (forked)/src/components/App.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); //arrray destructuring, because use state returns and saves an arrey to const
 
   function increase() {
     setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
   }
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
