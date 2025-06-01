44136011db1a786fb504c794f91248885d3bd0cb
Wooltrod <ianadoyo64@gmail.com>
2025-06-01 15:09:02 +0200

    Update h1 based on input ONLY after hitting submit button

--- 36.35 - React Forms (forked)/src/components/App.jsx
+++ 36.35 - React Forms (forked)/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+/*part 1: Rendering h1 dynamically based on input (onChange) event*/
 function App() {
   const [name, setName] = useState("");
 
@@ -19,5 +20,34 @@ function App() {
     </div>
   );
 }
+
+/*part 2: Update h1 based on input ONLY after hitting submit button*/
+
+function App() {
+  const [name, setName] = useState("");
+  const [isClicked, setClicked] = useState(false);
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setClicked(true);
+  }
+
+  return (
+    <div className="container">
+      {isClicked ? <h1>Hello {name}</h1> : <h1>Hello</h1>}
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
+}
+
 
 export default App;
