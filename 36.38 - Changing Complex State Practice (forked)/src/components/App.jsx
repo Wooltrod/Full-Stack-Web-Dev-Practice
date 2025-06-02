8a5950981fd7d261505170d71cd2cc426f87d10f
Wooltrod <ianadoyo64@gmail.com>
2025-06-03 00:24:28 +0200

    Complete 'Changing Complex State Practice' challenge

--- 36.38 - Changing Complex State Practice (forked)/src/components/App.jsx
+++ 36.38 - Changing Complex State Practice (forked)/src/components/App.jsx
@@ -4,8 +4,35 @@ function App() {
   const [contact, setContact] = useState({
     fName: "",
     lName: "",
-    email: ""
+    email: "",
   });
+
+  function handleInputs(event) {
+    const inputValue = event.target.value;
+    const inputName = event.target.name;
+
+    setContact((prevStateValue) => {
+      if (inputName === "fName") {
+        return {
+          fName: inputValue,
+          lName: prevStateValue.lName,
+          email: prevStateValue.email,
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevStateValue.fName,
+          lName: inputValue,
+          email: prevStateValue.email,
+        };
+      } else if (inputName === "email") {
+        return {
+          fName: prevStateValue.fName,
+          lName: prevStateValue.lName,
+          email: inputValue,
+        };
+      }
+    });
+  }
 
   return (
     <div className="container">
@@ -14,9 +41,24 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input
+          onChange={handleInputs}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleInputs}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleInputs}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
         <button>Submit</button>
       </form>
     </div>
