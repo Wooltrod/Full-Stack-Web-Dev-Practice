a6a815e34535a71113d67cdf34dbdb7e1e3f5faf
Wooltrod <ianadoyo64@gmail.com>
2025-06-02 21:52:45 +0200

    Challenge: Stateful rendering with TWO input boxes instead of one. Two separate event listener functions used.

--- 36.37 - Changing Complex State (forked)/src/components/App.jsx
+++ 36.37 - Changing Complex State (forked)/src/components/App.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+
+  function handleChange(event) {
+    setFname(event.target.value);
+  }
+
+  function handleChangeTwo(event) {
+    setLname(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fname} {lname}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} name="fName" placeholder="First Name" />
+        <input
+          onChange={handleChangeTwo}
+          name="lName"
+          placeholder="Last Name"
+        />
         <button>Submit</button>
       </form>
     </div>
