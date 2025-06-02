cd3667523f4d21da6d2c19575add5793fdf28335
Wooltrod <ianadoyo64@gmail.com>
2025-06-02 22:57:49 +0200

    Add useState/Stateful implementation that parses both fname and lname into a single object, and requires only one event listener function.

--- 36.37 - Changing Complex State (forked)/src/components/App.jsx
+++ 36.37 - Changing Complex State (forked)/src/components/App.jsx
@@ -33,4 +33,63 @@ function App() {
   );
 }
 
-export default App;
+function App() {
+  // const [fname, setFname] = useState("");
+  // const [lname, setLname] = useState("");
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+  });
+
+  // function handleChange(event) {
+  //   setFname(event.target.value);
+  // }
+
+  // function handleChangeTwo(event) {
+  //   setLname(event.target.value);
+  // }
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setFullName((prevValue) => {
+      if (inputName === "fName") {
+        return {
+          fname: newValue,
+          lname: prevValue.lname,
+        };
+      } else if (inputName === "lName") {
+        return {
+          fname: prevValue.fname,
+          lname: newValue,
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.fname} {fullName.lname}
+      </h1>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.fname}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lname}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App; 
