06d3449d79df388d90e0a128bbabb49bf24209a1
Wooltrod <ianadoyo64@gmail.com>
2025-06-03 21:59:40 +0200

    Step 3. Declare useState hook for array of item elements, declare map function we will use to render each item from our array as a list item

--- 36.40 - ES6 Spread Operator Practice (forked)/src/components/App.jsx
+++ 36.40 - ES6 Spread Operator Practice (forked)/src/components/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function App() {
 
   const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
     const newValue = event.target.value;
@@ -22,7 +23,9 @@ function App() {
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((toDoItem) => {
+            return <li>{toDoItem}</li>;
+          })}
         </ul>
       </div>
     </div>
