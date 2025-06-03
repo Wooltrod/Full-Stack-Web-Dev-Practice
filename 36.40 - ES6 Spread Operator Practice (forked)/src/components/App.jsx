ef8e8ea8549b5a401c2dc85f684cc665797007cb
Wooltrod <ianadoyo64@gmail.com>
2025-06-03 21:44:29 +0200

    Step 1. Add useState() hook for input element, to keep track/save the state of user input. Make sure to define value attribute of <input> element as {inputText}

--- 36.40 - ES6 Spread Operator Practice (forked)/src/components/App.jsx
+++ 36.40 - ES6 Spread Operator Practice (forked)/src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input type="text" value={inputText}/>
         <button>
           <span>Add</span>
         </button>
