d7ae85e42d7bc3b7923c773228d604baa92ffc8f
Wooltrod <ianadoyo64@gmail.com>
2025-06-04 23:24:17 +0200

    Mini Challenge: Implement State in the ToDoItem, crossing out the item when clicked. Create 'isDone' boolean variable + setIsDone state setter. Implement ternary operator to check for boolean state befor applying textDecoration styling.

--- 36.41 - Managing A Component Tree (forked)/src/components/ToDoItem.jsx
+++ 36.41 - Managing A Component Tree (forked)/src/components/ToDoItem.jsx
@@ -1,7 +1,25 @@
 import React, { useState } from "react";
 
 function ToDoItem(props) {
-  return <li>{props.item}</li>;
+  const [isDone, setIsDone] = useState(false);
+
+  function handleClick() {
+    setIsDone((prevValue) => {
+      return !prevValue;
+    });
+  }
+
+  return (
+    <div onClick={handleClick}>
+      <li
+        style={{
+          textDecoration: isDone ? "line-through" : "none",
+        }}
+      >
+        {props.item}
+      </li>
+    </div>
+  );
 }
 
 export default ToDoItem;
