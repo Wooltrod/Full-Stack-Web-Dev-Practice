9d1fa90f6bafc64c0698ddc66cd3e9118cb38479
Wooltrod <ianadoyo64@gmail.com>
2025-06-05 10:16:44 +0200

    Complete 'Managing A Component Tree Practice' challenge. Correctly pass over functions and variables to the InputArea component as props.

--- 36.42 - Managing A Component Tree Practice (forked)/src/components/InputArea.jsx
+++ 36.42 - Managing A Component Tree Practice (forked)/src/components/InputArea.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function InputArea() {
+function InputArea(props) {
   return (
     <div className="form">
-      <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
+      <input onChange={props.onInput} type="text" value={props.inputValue} />
+      <button onClick={props.onPressed}>
         <span>Add</span>
       </button>
     </div>
