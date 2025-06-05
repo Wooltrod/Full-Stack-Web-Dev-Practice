fb88f3199b2174a69df0242ad26c676aa2753af1
Wooltrod <ianadoyo64@gmail.com>
2025-06-05 09:04:39 +0200

    Update App.jsx & ToDoItem.jsx to enable deletion of list items on click

--- 36.41 - Managing A Component Tree (forked)/src/components/ToDoItem.jsx
+++ 36.41 - Managing A Component Tree (forked)/src/components/ToDoItem.jsx
@@ -1,23 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ToDoItem(props) {
-  const [isDone, setIsDone] = useState(false);
-
-  function handleClick() {
-    setIsDone((prevValue) => {
-      return !prevValue;
-    });
-  }
+  function handleClick() {}
 
   return (
-    <div onClick={handleClick}>
-      <li
-        style={{
-          textDecoration: isDone ? "line-through" : "none",
-        }}
-      >
-        {props.item}
-      </li>
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.item}</li>
     </div>
   );
 }
