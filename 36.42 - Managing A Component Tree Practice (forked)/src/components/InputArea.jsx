446e12d72dc3d90f9589af00f07282b85625e126
Wooltrod <ianadoyo64@gmail.com>
2025-06-06 11:10:06 +0200

    Step 1. Move over relevant stateful variables & the handleChange() function into the InputArea component.

--- 36.42 - Managing A Component Tree Practice (forked)/src/components/InputArea.jsx
+++ 36.42 - Managing A Component Tree Practice (forked)/src/components/InputArea.jsx
@@ -16,6 +16,13 @@ function InputArea(props) {
 import React, { useState } from "react";
 
 function InputArea() {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
   return (
     <div className="form">
       <input onChange={handleChange} type="text" value={inputText} />
