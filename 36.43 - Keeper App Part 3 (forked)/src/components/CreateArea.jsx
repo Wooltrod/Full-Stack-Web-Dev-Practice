30e7da7206602d32c0492ba66f5cecec1f25933a
Wooltrod <ianadoyo64@gmail.com>
2025-06-06 15:08:41 +0200

    Step 1. Define useState for the <CreateArea/> component. Initialise the user input as an empty object of fields 'title' & 'content' corresponding with the title and body of the note respectively.

--- 36.43 - Keeper App Part 3 (forked)/src/components/CreateArea.jsx
+++ 36.43 - Keeper App Part 3 (forked)/src/components/CreateArea.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea() {
+
+  const [noteObject, setNoteObject] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const inputValue = event.target.value;
+    const inputName = event.target.name;
+
+    setNoteObject((prevStateValue) => {
+      if (inputName === "title") {
+        return {
+          title: inputValue,
+          content: prevStateValue.content,
+        };
+      } else if (inputName === "content") {
+        return {
+          title: prevStateValue.title,
+          content: inputValue,
+        };
+      }
+    });
+  }
+
   return (
     <div>
       <form>
