48d5f80d946e07f77ae105a4846dcf277b1e5e47
Wooltrod <ianadoyo64@gmail.com>
2025-06-20 19:53:59 +0200

    Function with void return type (writes to console)

--- 37.2 - Typescript Functions/functions.ts
+++ 37.2 - Typescript Functions/functions.ts
@@ -7,5 +7,10 @@ const addStrings = (str1: string, str2: string = ""): string => `${str1} ${str2}
 //function with Union Typing (A parameter can be one of two types)
 const format = (title: string, param2: string | number): string => `${title} ${param2}`;
 
+//Function with void return type (writes to console)
+const printFormat = (title: string, param2: string | number): void => {
+    console.log(format(title, param2));
+};
+
 export default addNumbers;
 export { addStrings };
