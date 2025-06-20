eeeb45d75eef216fadfea1115accc54b4a535c0c
Wooltrod <ianadoyo64@gmail.com>
2025-06-20 19:50:40 +0200

    function with Union Typing (A parameter can be one of two types)

--- 37.2 - Typescript Functions/functions.ts
+++ 37.2 - Typescript Functions/functions.ts
@@ -4,5 +4,8 @@ function addNumbers(a:number, b:number): number {
 
 const addStrings = (str1: string, str2: string = ""): string => `${str1} ${str2}`;
 
+//function with Union Typing (A parameter can be one of two types)
+const format = (title: string, param2: string | number): string => `${title} ${param2}`;
+
 export default addNumbers;
 export { addStrings };
